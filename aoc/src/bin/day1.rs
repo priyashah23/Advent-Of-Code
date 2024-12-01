1284daa35cab85c6c758a8ab76b5c30294e60bad
Priya Shah <priya.shah2309@gmail.com>
2024-12-01 10:22:32 +0000

    day one part one solution

---
+++ aoc/src/bin/day1.rs
@@ -0,0 +1,44 @@
+use std::io::{self, BufRead};
+use std::fs::File;
+
+fn main()  -> io::Result<()>{
+    let file = File::open("")?;
+    let reader = io::BufReader::new(file);
+
+    let mut list_one: Vec<i64> = Vec::new();    
+    let mut list_two: Vec<i64> = Vec::new();
+
+    for line in reader.lines() {
+        let current_line = line?;
+        let mut iter = current_line.split_whitespace();
+        
+        match iter.next() {
+            Some(x) => {
+                let item = x.parse::<i64>().unwrap();
+                list_one.push(item);
+            },
+            None => {},
+        };
+
+        match iter.next() {
+            Some(y) => {
+                let item = y.parse::<i64>().unwrap();
+                list_two.push(item);
+            },
+            None => {},
+        };
+    }
+
+    list_one.sort();
+    list_two.sort();
+
+    let mut total_distance: i64 = 0;
+    let mut index= 0;
+    for line in list_one.iter() {
+        let distance = list_two.get(index).unwrap() - line;
+        total_distance += distance.abs();
+        index+=1;
+    }
+    print!("{}", total_distance);
+    Ok(())
+}
