d9380826fd01ee93d6ad51fa60def5f8b7edc69d
Priya Shah <priya.shah2309@gmail.com>
2024-12-02 20:13:04 +0000

    part2 solution

--- aoc/src/bin/day2.rs
+++ aoc/src/bin/day2.rs
@@ -1,8 +1,9 @@
 use std::io::{self, BufRead};
 use std::fs::File;
 
+
 fn main()  -> io::Result<()>{
-    let file = File::open("")?;
+    let file = File::open("/Users/priyashah/Documents/Personal Programming Project/Advent-Of-Code/aoc/src/files/input2.txt")?;
     let reader = io::BufReader::new(file);
 
     let mut safe_total = 0;
@@ -10,7 +11,8 @@ fn main()  -> io::Result<()>{
     for line in reader.lines() {
         let current_line = line.unwrap();
         let iter = current_line.split_whitespace();
-        
+
+        let mut mistakes: i32 = 0;
         let mut tuple = (-1, "BASE");
         let mut safe = true;
 
@@ -21,20 +23,32 @@ fn main()  -> io::Result<()>{
                 continue;
             } else {
                 if (tuple.0 - item).abs() > 3 {
-                    safe = false;
-                    break;
+                    if mistakes >= 1 {
+                        safe = false;
+                        break;
+                    }
+                    mistakes += 1;
+                    continue;
                 }
                 if tuple.0 == item {
-                    safe = false;
-                    break;
+                    if mistakes >= 1 {
+                        safe = false;
+                        break;
+                    }
+                    mistakes += 1;
+                    continue;
                 }
                 if tuple.0 > item {
                     if tuple.1 == "BASE" {
                         tuple.1 = "DECREASE";
                     }
                     else if tuple.1 == "INCREASE" {
-                        safe = false;
-                        break;
+                        if mistakes >= 1 {
+                            safe = false;
+                            break;
+                        }
+                        mistakes += 1;
+                        continue;
                     }
                 } 
                 if tuple.0 < item {
@@ -42,8 +56,12 @@ fn main()  -> io::Result<()>{
                         tuple.1 = "INCREASE";
                     }
                     else if tuple.1 == "DECREASE" {
-                        safe = false;
-                        break;
+                        if mistakes >= 1 {
+                            safe = false;
+                            break;
+                        }
+                        mistakes += 1;
+                        continue;
                     }
                 }
                 tuple.0 = item;
